64b52f24c575e189e63cdde5be9094b3b96b2c53
cortesarts <dennis.cortes@satchelhealth.com>
2017-01-07 20:49:51 -0600

    Setting up usual structure for project

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div id="home" className="bg-black-05">
+        <h1>
+          Home
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
