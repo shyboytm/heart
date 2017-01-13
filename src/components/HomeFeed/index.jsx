f2eea3f08d9ec0c2ac363551d2777c3966eb7f79
cortesarts <dennis.cortes@satchelhealth.com>
2017-01-12 19:28:40 -0600

    Adds propTypes to HomeFeedItem component and changes max-width for body

--- src/components/HomeFeed/index.jsx
+++ src/components/HomeFeed/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import HomeFeedItem from '../HomeFeedItem';
-import P from '../type-elements/P';
 import H from '../type-elements/H';
 
 class HomeFeed extends Component {
@@ -9,9 +8,30 @@ class HomeFeed extends Component {
     return (
       <div id="HomeFeed" className="">
         <H size={4} tt="u">Latest</H>
-        <HomeFeedItem />
-        <HomeFeedItem />
-        <HomeFeedItem />
+        <HomeFeedItem
+          postTitle="Tips for Working Remote (Part 2)"
+          volumeNumber="18"
+          publishedDate="Jan 12 2017" />
+        <HomeFeedItem
+          postTitle="Tips for Working Remote (Part 1)"
+          volumeNumber="17"
+          publishedDate="Jan 5 2017" />
+        <HomeFeedItem
+          postTitle="My 2016"
+          volumeNumber="16"
+          publishedDate="Dec 29 2016" />
+        <HomeFeedItem
+          postTitle="Distractions Provide Progress"
+          volumeNumber="15"
+          publishedDate="Dec 22 2016" />
+        <HomeFeedItem
+          postTitle="How to Cure Creative Block"
+          volumeNumber="14"
+          publishedDate="Dec 15 2016" />
+        <HomeFeedItem
+          postTitle="People are More than Connections"
+          volumeNumber="13"
+          publishedDate="Dec 8 2016" />
       </div>
     );
   }
--- src/components/HomeFeedItem/index.jsx
+++ src/components/HomeFeedItem/index.jsx
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 import H from '../type-elements/H';
-import P from '../type-elements/P';
 
 class HomeFeedItem extends Component {
   render() {
+
+    const {
+      postTitle,
+      volumeNumber,
+      publishedDate
+    } = this.props
+
     return (
       <Link to="/" className="db pointer link mb4">
         <hr className="b--black-05 mb3" />
         <div className="forward">
-          <H size={1} color="black">People are More than Connections</H>
-          <span className="blue pr3">Volume 13</span>
-          <span className="black-50">Published Dec 8 2016</span>
+          <H size={1} color="black">{postTitle}</H>
+          <span className="blue pr3">Volume {volumeNumber}</span>
+          <span className="black-50">Published {publishedDate}</span>
         </div>
       </Link>
     );
