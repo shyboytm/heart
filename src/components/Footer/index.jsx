6feaa036daba4c49726b5178fd2b1de93bacc300
cortesarts <dennis.cortes@satchelhealth.com>
2017-01-12 20:50:03 -0600

    Adds titles of posts for home feed

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,7 +6,7 @@ class Footer extends Component {
     const blackLogo =     require('../../img/roc-logo.png');
 
     return (
-      <div id="Footer" className="pv4 black-30">
+      <div id="Footer" className="pv5 black-30">
         <img src={blackLogo} alt="Rate of Change Logo" className="db h3 center mb3 tc"/>
         <small className="f6 db tc">© 2017 <b>Dennis Cortés</b>, All Rights Reserved</small>
         <div className="tc mt3">
--- src/components/HomeFeed/index.jsx
+++ src/components/HomeFeed/index.jsx
@@ -32,6 +32,58 @@ class HomeFeed extends Component {
           postTitle="People are More than Connections"
           volumeNumber="13"
           publishedDate="Dec 8 2016" />
+        <HomeFeedItem
+          postTitle="Less Perfection and More Iteration"
+          volumeNumber="12"
+          publishedDate="Dec 1 2016" />
+        <HomeFeedItem
+          postTitle="Advantages of Side Projects"
+          volumeNumber="11"
+          publishedDate="Nov 24 2016" />
+        <HomeFeedItem
+          postTitle="Places to Help You Stay Inspired"
+          volumeNumber="10"
+          publishedDate="Nov 17 2016" />
+        <HomeFeedItem
+          postTitle="Why Vacations are Important"
+          volumeNumber="9"
+          publishedDate="Nov 10 2016" />
+        <HomeFeedItem
+          postTitle="How to Get Better"
+          volumeNumber="8"
+          publishedDate="Nov 3 2016" />
+        <HomeFeedItem
+          postTitle="How to Accept and Use Feedback"
+          volumeNumber="7"
+          publishedDate="Oct 27 2016" />
+        <HomeFeedItem
+          postTitle="Break Your Comfort Zone"
+          volumeNumber="6"
+          publishedDate="Oct 20 2016" />
+        <HomeFeedItem
+          postTitle="Budgeting Your Bandwidth"
+          volumeNumber="5"
+          publishedDate="Oct 13 2016" />
+        <HomeFeedItem
+          postTitle="Why You Should Seek Boredom"
+          volumeNumber="4"
+          publishedDate="Oct 6 2016" />
+        <HomeFeedItem
+          postTitle="Why You Should Seek Boredom"
+          volumeNumber="4"
+          publishedDate="Oct 6 2016" />
+        <HomeFeedItem
+          postTitle="The Advantages of Failing"
+          volumeNumber="3"
+          publishedDate="Sep 29 2016" />
+        <HomeFeedItem
+          postTitle="Trends Fade, Technique Does Not"
+          volumeNumber="2"
+          publishedDate="Sep 22 2016" />
+        <HomeFeedItem
+          postTitle="Introducing: The Rate of Change Blog"
+          volumeNumber="1"
+          publishedDate="Sep 15 2016" />
       </div>
     );
   }
