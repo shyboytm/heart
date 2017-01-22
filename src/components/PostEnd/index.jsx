c9c99f2d278aadb61adce0a8683795bfbd0f0060
cortesarts <dennis.cortes@satchelhealth.com>
2017-01-22 15:48:15 -0600

    Finishes Eighteen post

--- src/components/PostEnd/index.jsx
+++ src/components/PostEnd/index.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 
 import P from '../type-elements/P';
+import L from '../type-elements/L';
 
 class PostEnd extends Component {
   render() {
     return (
       <div id="PostEnd" className="mt5">
-        <P><em>I’d love for you to follow the blog here on Medium or join my personal newsletter. Feel free to reach out to me on Twitter if you have any feedback, questions, or just to say hello!</em><br/>
+        <P><em>I’d love for you to follow the blog on <L hrefLink="https://medium.com/the-rate-of-change">Medium</L> or join my personal newsletter. Feel free to reach out to me on <L hrefLink="https://twitter.com/fromcortes">Twitter</L> if you have any feedback, questions, or just to say hello!</em><br/>
           <b className="mt4 mb0 db black">— Dennis Cortés</b><br/>
         </P>
       </div>
