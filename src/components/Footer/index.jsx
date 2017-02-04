19f1a1baa4c14c67eba2e487081068a8e1984173
fromcortes <dennis.cortes@satchelhealth.com>
2017-02-04 12:02:52 -0600

    Adds Zorua to Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
 
-    const blackLogo =     require('../../img/roc-logo.png');
+    const blackLogo = require('../../img/roc-logo.png');
+    const zorua     = require('../../img/zorua.gif');
 
     return (
-      <div id="Footer" className="pv2 mt4">
+      <div id="Footer" className="pv2 mt4 center tc">
         <img src={blackLogo} alt="Rate of Change Logo" className="db h3 center mb3 tc"/>
         <div className="tc mt3 mb4">
           <a href="http://cortes.us" title="Website" className="f6 dib ph2 link blue b hover-light-blue">Website</a>
@@ -15,6 +16,7 @@ class Footer extends Component {
           <a href="#" title="Contact" className="f6 dib ph2 link blue b hover-light-blue">Contact</a>
         </div>
         <small className="f6 db tc f7 black-20">© 2017 <b>Dennis Cortés</b>, All Rights Reserved</small>
+        <img className="w2 mt4" src={zorua} />
       </div>
     );
   }
