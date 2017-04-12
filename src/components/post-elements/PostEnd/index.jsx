2acba928f9d78a2f485454464f78dc738f990453
Dennis Cortés <hi@cortes.us>
2017-04-12 15:33:25 -0500

    Updates Footer to have read more CTA

--- src/components/post-elements/PostEnd/index.jsx
+++ src/components/post-elements/PostEnd/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 import L from '../../type-elements/L';
 import LineBreak from '../../type-elements/LineBreak';
@@ -12,11 +13,10 @@ class PostEnd extends Component {
         <b className="db f4 black tc">— Dennis Cortés</b><br/>
         <LineBreak />
         <div className="tc center">
-          <a className="mt4 f6 link hover-bg-light-red br3 ph3 pv2 mb2 dib white bg-red" href="http://eepurl.com/cgT2bD">Subscribe to Newsletter</a>
+          <a className="mt4 f6 link hover-bg-light-red br3 ph3 pv2 mb2 dib white bg-red mr3-ns" href="http://eepurl.com/cgT2bD">Subscribe to Newsletter</a>
+          <Link className="mt4 f6 link hover-bg-light-blue br3 ph3 pv2 mb2 dib white bg-blue" to="/">Read More Articles</Link>
         </div>
-        <P><em>I’d love for you to follow the blog on <L hrefLink="https://medium.com/the-rate-of-change">Medium</L> or join my personal newsletter. Feel free to reach out to me on <L hrefLink="https://twitter.com/fromcortes">Twitter</L> if you have any feedback, questions, or just to say hello!</em><br/>
-
-        </P>
+        <P><em>I’d love for you to follow the blog on <L hrefLink="https://medium.com/the-rate-of-change">Medium</L> or join my <L hrefLink="http://eepurl.com/cgT2bD">personal newsletter</L>. Feel free to reach out to me on <L hrefLink="https://twitter.com/fromcortes">Twitter</L> if you have any feedback, questions, or just to say hello!</em><br/></P>
       </div>
     );
   }
