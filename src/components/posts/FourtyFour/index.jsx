39c3e43c947b94ed35035530d3c925c22c578547
Dennis Cortés <hi@cortes.us>
2017-07-14 15:20:00 -0500

    Adds FourtyFour post

--- src/components/posts/FourtyFour/index.jsx
+++ src/components/posts/FourtyFour/index.jsx
@@ -17,15 +17,15 @@ import Social from '../../type-elements/Social';
 class FourtyFour extends Component {
   render() {
 
-    const imgFourtyFour = require('../../../img/roc-volume-43-cover.png');
+    const imgFourtyFour = require('../../../img/roc-volume-44-cover.png');
 
     return (
       <div className="cf w-100">
         <Header />
         <Social
-          twitterLink="https://twitter.com/home?status=Why%20You%20Should%20Start%20Writing%20by%20%40fromcortes%20http%3A//blog.cortes.us/why-you-should-start-writing"
-          facebookLink="https://www.facebook.com/sharer/sharer.php?u=http%3A//blog.cortes.us/why-you-should-start-writing"
-          googleLink="https://plus.google.com/share?url=http%3A//blog.cortes.us/why-you-should-start-writing" />
+          twitterLink="https://twitter.com/home?status=How%20to%20Make%20Passive%20Social%20Media%20Traction%20by%20%40fromcortes%20http%3A//blog.cortes.us/how-to-make-passive-social-media-traction"
+          facebookLink="https://www.facebook.com/sharer/sharer.php?u=http%3A//blog.cortes.us/how-to-make-passive-social-media-traction"
+          googleLink="https://plus.google.com/share?url=http%3A//blog.cortes.us/how-to-make-passive-social-media-traction" />
         <div id="44" className="bg-white br3 ph5-l pv4-l ph4 pv3 shadow-custom">
 
           <Image imgSrc={imgFourtyFour} imgAlt="How to Make Passive Social Media Traction" />
@@ -37,7 +37,11 @@ class FourtyFour extends Component {
 
           <P>Welcome to Volume 44 of The Rate of Change. I hope you’re having an amazing week. As I promised, each of these will have a main blog post, and an update about my professional and personal life for this week. So before I get into this week’s article, here’s a quick update on my week.</P>
           <H size={2}>Side Projects - <L hrefLink="http://usequarry.com">usequarry.com</L></H>
+          <P>This week I’ve still been working on <L hrefLink="https://livtra.co">Livtra</L> and <L hrefLink="https://gamesandgrids.com">Games and Grids</L>. The overall look and feel and basic dashboard for Livtra is in place currently and now Aaron and I are working on implementing features and improving the experience of the app little by little. We’re still hoping for a beta launch at the end of September so make sure you’re signed up for updates!</P>
+          <P>Nothing too crazy, but I shared a sneak peek of part of Livtra’s dashboard on Twitter this week <L hrefLink="https://twitter.com/fromcortes/status/885557028546117634">here</L> if you are interested.</P>
           <H size={2}>Personal Life</H>
+          <P>I have been pretty busy in my personal life this week. My girlfriend moved this week before I do at the end of the month so it feels a bit weird to be by myself for the time being. Since I was helping her get packed, I was a little MIA overall and hence why this article is a day late. I am excited to be productive while also making time for video games, but it’s definitely different since her and I are usually together all the time.</P>
+          <P>I know I wrote about this last week, but I’m still amazed at how great Zelda Breath of the Wild is. Never have I put 90+ hours into a video game and still felt as excited and determined as I do every time I start up that game. I still find so many great aspects of story, adventure, discovery, and characters and I’ve only completed ~30% of the game. You need to do yourself a favor and play it at some point in your life.</P>
 
           <LineBreak />
 
