8f6cc525f72a54cad79c55fc6fa095dab6a28217
fromcortes <hi@cortes.us>
2017-08-05 14:59:51 -0500

    Improves logo look, updates Header links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,14 +8,14 @@ class Footer extends Component {
 
     return (
       <div id="Footer" className="flex flex-row-l flex-column items-center justify-between-l tc pa4 bt b--black-05">
-        <h1 className="flex f4 ttu fw9 tracked mv0-ns mb3">The Rate of Change</h1>
+        <h1 className="flex f4 lh-title b avenir mv0-l mb3">The Rate <em className="georgia pl1 pr2 fw4 black-30">of</em> Change</h1>
         <div className="flex tc mv0-l mv3">
           <a href="http://cortes.us" title="Website" target="_blank" className="f6 dib ph2 link blue b hover-light-blue">Website</a>
           <a href="http://eepurl.com/cgT2bD" title="Subscribe" target="_blank" className="f6 dib ph2 link blue b hover-light-blue">Subscribe</a>
           <a href="mailto:hi@cortes.us?subject=Sponsoring The Rate of Change" title="Sponsor" target="_blank" className="f6 dib ph2 link blue b hover-light-blue">Sponsor</a>
           <a href="mailto:hi@cortes.us" title="Contact" target="_blank" className="f6 dib ph2 link blue b hover-light-blue">Contact</a>
         </div>
-        <small className="flex f7 mv0-l mv4 black-20">© 2017<b className="pl1">Dennis Cortés</b></small>
+        <small className="flex f7 mv0-l mv4 black-20">© 2017 Dennis Cortés</small>
         <img className="flex w2" src={gengar} />
       </div>
     );
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,12 +7,12 @@ class Header extends Component {
       <div id="Header" className="flex flex-row-l flex-column items-center justify-between w-100 pa4 bb b--black-05 lh-copy">
         <div className="flex">
           <Link to="/" className="link black hover-black-30">
-            <h1 className="f4 lh-title ttu fw9 tracked mv0-l mb3">The Rate of Change</h1>
+            <h1 className="f4 lh-title b mv0-l mb3 avenir">The Rate<br /><em className="georgia ml4 pl2 fw4 black-30">of</em> Change</h1>
           </Link>
         </div>
         <div className="flex items-center db">
-          <a href="/" className="f6 link black hover-blue mr4">by Dennis Cortés</a>
-          <a href="/" className="f6 link bg-animate hover-bg-light-red br2 ph2 pv1 dib white bg-red">Subscribe</a>
+          <a target="_blank" href="https://cortes.us" className="f6 link black hover-blue mr4">by Dennis Cortés</a>
+          <a target="_blank" href="http://eepurl.com/cgT2bD" className="f6 link bg-animate hover-bg-light-red br2 ph2 pv1 dib white bg-red">Subscribe</a>
         </div>
       </div>
     );
