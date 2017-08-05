723eeba8cdd66ad71bc21b4c416deca7651ba027
fromcortes <hi@cortes.us>
2017-08-05 11:28:30 -0500

    Improves responsiveness for Header and Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,16 +7,16 @@ class Footer extends Component {
     const gengar    = require('../../img/gengar.gif');
 
     return (
-      <div id="Footer" className="flex-l db items-center justify-between-l tc pa4 bt b--black-05">
-        <h1 className="flex-l db f4 ttu fw9 tracked mv0-ns mb3">The Rate of Change</h1>
-        <div className="flex-l db tc mv0-l mv3">
+      <div id="Footer" className="flex flex-row-l flex-column items-center justify-between-l tc pa4 bt b--black-05">
+        <h1 className="flex f4 ttu fw9 tracked mv0-ns mb3">The Rate of Change</h1>
+        <div className="flex tc mv0-l mv3">
           <a href="http://cortes.us" title="Website" target="_blank" className="f6 dib ph2 link blue b hover-light-blue">Website</a>
           <a href="http://eepurl.com/cgT2bD" title="Subscribe" target="_blank" className="f6 dib ph2 link blue b hover-light-blue">Subscribe</a>
           <a href="mailto:hi@cortes.us?subject=Sponsoring The Rate of Change" title="Sponsor" target="_blank" className="f6 dib ph2 link blue b hover-light-blue">Sponsor</a>
           <a href="mailto:hi@cortes.us" title="Contact" target="_blank" className="f6 dib ph2 link blue b hover-light-blue">Contact</a>
         </div>
-        <small className="flex-l db f7 mv0-l mv4 black-20">© 2017<b className="pl1">Dennis Cortés</b></small>
-        <img className="flex-l db center w2" src={gengar} />
+        <small className="flex f7 mv0-l mv4 black-20">© 2017<b className="pl1">Dennis Cortés</b></small>
+        <img className="flex w2" src={gengar} />
       </div>
     );
   }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router';
 class Header extends Component {
   render() {
     return (
-      <div id="Header" className="flex-ns items-center justify-between w-100 pa4 tl-ns tc bb b--black-05 lh-copy">
-        <div className="flex-ns db">
+      <div id="Header" className="flex flex-row-l flex-column items-center justify-between w-100 pa4 bb b--black-05 lh-copy">
+        <div className="flex">
           <Link to="/" className="link black hover-black-30">
-            <h1 className="dib f4 lh-title ttu fw9 tracked mv0-ns mb3">The Rate of Change</h1>
+            <h1 className="f4 lh-title ttu fw9 tracked mv0-l mb3">The Rate of Change</h1>
           </Link>
         </div>
-        <div className="flex-ns items-center db">
+        <div className="flex items-center db">
           <a href="/" className="f6 link black hover-blue mr4">by Dennis Cortés</a>
           <a href="/" className="f6 link bg-animate hover-bg-light-red br2 ph2 pv1 dib white bg-red">Subscribe</a>
         </div>
